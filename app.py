4aad45d9436c97c7950600afd43593d0b853cd0f
ghost-mann <iamaob8@protonmail.com>
2025-07-01 12:26:06 +0300

    feat: added auth.py and models.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import render_template, Flask, request
+from flask import render_template, Flask, request, redirect, url_for,flash, Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 from dotenv import load_dotenv
+from models import User,db
 import os
 
 
